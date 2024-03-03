0ed111c22bd5f195e48a4558607d92a6125f6d47
Abdul-Moiz12 <154911874+Abdul-Moiz12@users.noreply.github.com>
2024-03-03 12:47:17 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+//Alert Questions
+
+//1. Write a script to greet your website visitor using JS alert box.
+
+var greet = 'Hello Gentleman';
+alert(greet);
+
+//2. Write a script to display following message on your web page:
+
+var error = "Error! Please enter a valid password";
+alert(error);
+
+//3. Write a script to display following message on your web page: (Hint : Use line break)
+
+var msg = "Welcome to JS Land <br/> Happy Coding!";
+document.write(msg);
+
+//4. Write a script to display following messages in sequence:
+
+var msg1 = "Welcome to JS Land";
+alert(msg1);
+
+var msg2 = "Happy Coding!";
+alert(msg2);
+
+//5. Generate the following message through browser’s developer console:
+
+var message = "Hello...I can run JS through my web browser's console";
+console.log(message);
+
+//6. Make use of alerts in your new/existing HTML & CSS project.
+
+var marks1 = 34;
+var marks2 = 55;
+var marks3 = 67;
+var result = marks1 + marks2 + marks3;
+ console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
